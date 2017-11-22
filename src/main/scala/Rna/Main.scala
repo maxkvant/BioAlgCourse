5b8f3f7b61b2424df466e61958ff13ec68652025
maxkvant <max.kvant@gmail.com>
2017-11-22 16:58:39 +0300

    fix

--- src/main/scala/Rna/Main.scala
+++ src/main/scala/Rna/Main.scala
@@ -2,11 +2,15 @@ package Rna
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val s = "AAAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCCCCCCCCCCUUUUUUUUUUUUUUUUUUUUUUUUUUU"
-    val t = "AAACAUGAGGAUUACCCAUGU"
-    val res = Nussinov(t)
-    println(res.length)
-    res.foreach({case (i, j) => println(s"${s(i)} ${i+1} ${j+1} ${s(j)}")})
-  }
+    def run(t: String) = {
+      val res = Nussinov(t)
+      println (res.length)
+      res.foreach ( {case (i, j) => println (s"${t (i)} ${i + 1} ${j + 1} ${t (j)}")})
+    }
 
+    val s = "AAAAAAAAAAAAAAAAAAAAAAAAAGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCCCCCCCCCCUUUUUUUUUUUUUUUUUUUUUUUUUUU"
+    val t = "AAACAUGAGGAUUACC"
+
+    run(t)
+  }
 }
